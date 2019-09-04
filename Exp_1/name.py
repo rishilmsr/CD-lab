03e86a122afeba284e166cb96769984cf52acfe5
rishilmsr <38310805+rishilmsr@users.noreply.github.com>
2019-09-04 16:16:31 +0530

    Add files via upload

---
+++ Exp_1/name.py
@@ -0,0 +1,32 @@
+'''				INPUT			'''
+
+print("hello")
+a = 5+4
+b = true
+c+=45
+a & b
+d = "good morning"
+
+
+'''				OUTPUT		
+
+
+			Keyword : print 
+		 string : "hello" 
+		Identifier a 
+		assignment operator : = 
+		Integer : 5 (5) 
+		 arithmetic operator : + 
+		Integer : 4 (4) 
+		 Identifier b 
+		assignment operator : = 
+		Identifier true 
+		Identifier c 
+		assignment operator : += 
+		Integer : 45 (45) 
+		 Identifier a 
+		bitwise operator : & 
+		Identifier b 
+		Identifier d 
+		assignment operator : = 
+		string : "good morning" 		'''
